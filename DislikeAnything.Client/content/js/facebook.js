16305e0bd544577f8b7e2f1c15c96354bacbc085
brandon fisch <brandon.fisch@gmail.com>
2011-10-21 18:07:07 -0700

    added method to check if a url is already disliked

--- DislikeAnything.Client/content/js/facebook.js
+++ DislikeAnything.Client/content/js/facebook.js
@@ -67,6 +67,25 @@ var facebook = {
 			});
 		},
 		
+	getPageDislikeStatus:
+		function(url, callback){
+			facebook.login(function(access_token){
+				endpoint = "https://graph.facebook.com/me/dislikeanything:not_like";
+				data = {"access_token" : access_token}
+				$.get(endpoint, data, function(response){
+					var dislikes = response.data;
+					var isDisliked = false;
+					for(var i in dislikes){
+						if(dislikes[i].data.website.url == url){
+							isDisliked = true;
+							break;
+						}
+					}
+					callback(isDisliked)
+				}, "json");
+			});
+		},
+		
 	dislike:
 		function(callback){
 			facebook.login(function(accessToken){
@@ -95,16 +114,16 @@ var facebook = {
             $.post(url, data, function (response) {
             	responseObject = JSON.parse(response);
             	if(responseObject.id){
-                    var notification = webkitNotifications.createNotification(
-                    'skin/icons/icon-48.png',  // icon url - can be relative
-                    'you dislike this',  // notification title
-                     data.graphObject  // notification body text
+            		var notification = webkitNotifications.createNotification(
+            			'../skin/icons/icon-48.png',  // icon url - can be relative
+            			'you dislike this',  // notification title
+            			data.graphObject  // notification body text
                     );
                     notification.ondisplay = function(){
-                    chrome.browserAction.setBadgeBackgroundColor({color: [59, 89 , 182 , 255]});
-                    chrome.browserAction.setBadgeText({text:"ugh"});
-                  }  
-                notification.show(); 
+                    	chrome.browserAction.setBadgeBackgroundColor({color: [59, 89 , 182 , 255]});
+                    	chrome.browserAction.setBadgeText({text:"ugh"});
+                    }  
+                    notification.show(); 
                	}else{
             		alert("error");
             		console.log(responseObject)
